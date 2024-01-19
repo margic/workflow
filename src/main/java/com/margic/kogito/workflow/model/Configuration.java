177f5cb5f46e904bd06cacb33b02e765422f815d
Paul Crofts <pmcrofts@margic.com>
2024-01-19 22:52:34 +0000

    stripped back to basic project

--- src/main/java/com/margic/kogito/workflow/model/Configuration.java
+++ src/main/java/com/margic/kogito/workflow/model/Configuration.java
@@ -1,7 +1,5 @@
 package com.margic.kogito.workflow.model;
 
-import java.io.Serializable;
-
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
@@ -9,6 +7,9 @@ import lombok.extern.jackson.Jacksonized;
 @Data
 @Builder
 @Jacksonized
-public class GenerateProtestsInput implements Serializable{
-    private int numberToGenerate;
+public class Configuration {
+
+    private String source;
+    private String destination;
+    
 }
--- src/main/java/com/margic/kogito/workflow/model/Session.java
+++ src/main/java/com/margic/kogito/workflow/model/Session.java
@@ -1,8 +1,24 @@
 package com.margic.kogito.workflow.model;
 
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
 /**
  * Session is object that is used to provide configuration tracing and control
  */
- public class Session {
 
- }
+
+@Data
+@Builder
+@Jacksonized
+public class Session {
+
+    private String Id;
+    private String type;
+    private Configuration config;
+
+
+}
